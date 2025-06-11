7fad2b2ad064dfb3674e5f8543d4106526f26515
Jesus David Quispe Quispe <jumbo7u7@gmail.com>
2025-06-10 20:35:35 -0500

    add nadvar and container

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'hola mundo', home: Container(child:  Text("Hola mundo"),));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'hola mundo',
+      home: RecipeBook(),
+    );
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Recipe Bokk', style: TextStyle(color: Colors.white)
+        ),
+      ),
+      body: Container(width: MediaQuery.of(context).size.width, height: 125, child:  Card(child: Row(children: <Widget> [
+        Container(
+          height: 125,
+          width: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container()
+          ),
+        ),
+        SizedBox(width: 26),
+        Column(children: <Widget> [
+          Text('Lasagna'),
+          Text('Jesud David'),
+          Container(
+            height: 2,
+            width: 75,
+            color: Colors.orange
+          )
+
+        ],)
+      ],),),),
+    );
   }
 }
