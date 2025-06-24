113ba668b01e7ecb4ea35ee59738c2ac102cc503
Jesus David Quispe Quispe <jumbo7u7@gmail.com>
2025-06-23 22:27:02 -0500

    add columns un app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,39 +8,51 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(children: <Widget> [
         _RecipesCard(context),
-        _RecipesCard(context)
+        _RecipesCard(context),
       ])
     );
   }
 
   Widget _RecipesCard(BuildContext context){
-    return Container(width: MediaQuery.of(context).size.width, height: 125, child:  Card(child: Row(children: <Widget> [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Container(
-              height: 125,
-              width: 100,
-
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container()
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width, 
+              height: 125, 
+              child:  Card(
+                child: Row(
+                  children: <Widget> [
+            Container (
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                height: 125,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.orange
+                ),
+      
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container()
+                ),
               ),
             ),
-          ),
-          SizedBox(width: 26),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget> [
-            Text('Lasagna'),
-            Text('Jesud David'),
-            Container(
-              height: 2,
-              width: 75,
-              color: Colors.orange
-            )
-      
-          ],)
-        ],),),);
+            SizedBox(width: 26),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget> [
+              Text('Lasagna'),
+              Text('Jesud David'),
+              Container(
+                height: 2,
+                width: 75,
+                color: Colors.orange
+              )
+        
+            ],)
+          ],),),),
+    );
   }
 }
